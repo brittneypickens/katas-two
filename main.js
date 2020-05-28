cf7ae3dcb5c495b24a174ea36798e482e0ddb8aa
brittney pickens <brittneypickens@brittneys-mbp.home>
2020-05-28 14:35:04 -0400

    just getting started

---
+++ main.js
@@ -0,0 +1,13 @@
+// Add
+function add(x, y) {
+    return x + y
+}
+console.log('Add:' , add(20, 45))
+
+// multiply
+
+function multiply(x) {
+for (let x = 0; x <= 25; x += 5)  {}  
+ 
+}
+console.log('Multiply:' , multiply(x = 25))

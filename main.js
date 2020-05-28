27bb5b62d97f2ae66fd95950d488e6889b2a55ef
brittney pickens <brittneypickens@brittneys-mbp.home>
2020-05-28 15:19:42 -0400

    first two done

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ console.log('Add:' , add(20, 45))
 
 function multiply(x) {
 for (let x = 0; x <= 25; x += 5)  {}  
- 
+ return x
 }
 console.log('Multiply:' , multiply(x = 25))
+
+// power
+function power () {
+    return
+}
+console.log('Power:' , power())
